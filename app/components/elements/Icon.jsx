273273ce2bb7c1f79ce14607e63b896109e911fd
kuissoft <fivewisdomzero@gmail.com>
2016-12-03 01:49:12 -0500

    new icons

--- app/components/elements/Icon.jsx
+++ app/components/elements/Icon.jsx
@@ -32,7 +32,8 @@ const icons = [
     'photo',
     'line',
     'video',
-    'eye',
+    'location',
+    'calendar',
 ];
 const icons_map = {};
 for (const i of icons) icons_map[i] = require(`app/assets/icons/${i}.svg`);
