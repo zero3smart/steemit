77be7bd6bc2047cd8c5eb05b7cd7aaf666b3279f
kuissoft <fivewisdomzero@gmail.com>
2017-03-07 00:25:33 -0500

    #1113 set state before return vote, misc eslint cleanup

--- app/redux/GlobalReducer.js
+++ app/redux/GlobalReducer.js
@@ -133,11 +133,12 @@ export default createModule({
                 const idx = active_votes.findIndex(v => v.get('voter') === username)
                 // steemd flips weight into percent
                 if(idx === -1)
-                    active_votes = active_votes.push(Map({voter: username, percent: weight}))
+                    active_votes = active_votes.push(Map({voter: username, percent: weight}));
                 else {
-                    active_votes = active_votes.set(idx, Map({voter: username, percent: weight}))
+                    active_votes = active_votes.set(idx, Map({voter: username, percent: weight}));
                 }
-                return state.setIn(key, active_votes)
+                state.setIn(key, active_votes);
+                return state;
             }
         },
         {
@@ -157,9 +158,7 @@ export default createModule({
                 let new_state;
                 if (order === 'by_author' || order === 'by_feed' || order === 'by_comments' || order === 'by_replies') {
                     // category is either "blog", "feed", "comments", or "recent_replies" (respectively) -- and all posts are keyed under current profile
-                    // one exception: "comments" category is keyed as "posts" in get_state (https://github.com/steemit/steem/issues/507)
-                    const _legacy_compat = state.getIn(['accounts', accountname]).has("posts") //TODO: remove after switching to shared-db
-                    const key = ['accounts', accountname, (_legacy_compat && category == "comments") ? "posts" : category]
+                    const key = ['accounts', accountname, category]
                     new_state = state.updateIn(key, List(), list => {
                         return list.withMutations(posts => {
                             data.forEach(value => {
