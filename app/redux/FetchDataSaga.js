c32aeb3026bf7de45ccd9e11a101570f386fb82d
kuissoft <fivewisdomzero@gmail.com>
2017-06-27 00:01:17 -0400

    add support for payout sort

--- app/redux/FetchDataSaga.js
+++ app/redux/FetchDataSaga.js
@@ -2,12 +2,12 @@ import {takeLatest, takeEvery} from 'redux-saga';
 import {call, put, select, fork} from 'redux-saga/effects';
 import {loadFollows, fetchFollowCount} from 'app/redux/FollowSaga';
 import {getContent} from 'app/redux/SagaShared';
+import Apis from 'shared/api_client/ApiInstances';
 import GlobalReducer from './GlobalReducer';
 import constants from './constants';
 import {fromJS, Map} from 'immutable'
-import {api} from 'steem';
 
-export const fetchDataWatches = [watchLocationChange, watchDataRequests, watchFetchJsonRequests, watchFetchState, watchGetContent];
+export const fetchDataWatches = [watchLocationChange, watchDataRequests, watchApiRequests, watchFetchJsonRequests, watchFetchState, watchGetContent];
 
 export function* watchDataRequests() {
     yield* takeLatest('REQUEST_DATA', fetchData);
@@ -28,8 +28,8 @@ export function* fetchState(location_change_action) {
     if(m && m.length === 2) {
         const username = m[1]
         yield fork(fetchFollowCount, username)
-        yield fork(loadFollows, "getFollowersAsync", username, 'blog')
-        yield fork(loadFollows, "getFollowingAsync", username, 'blog')
+        yield fork(loadFollows, "get_followers", username, 'blog')
+        yield fork(loadFollows, "get_following", username, 'blog')
     }
 
     // `ignore_fetch` case should only trigger on initial page load. No need to call
@@ -47,7 +47,8 @@ export function* fetchState(location_change_action) {
     if (url.indexOf("/author-rewards") !== -1) url = url.replace("/author-rewards", "/transfers");
 
     try {
-        const state = yield call([api, api.getStateAsync], url)
+        const db_api = Apis.instance().db_api;
+        const state = yield call([db_api, db_api.exec], 'get_state', [url]);
         yield put(GlobalReducer.actions.receiveState(state));
     } catch (error) {
         console.error('~~ Saga fetchState error ~~>', url, error);
@@ -72,100 +73,107 @@ export function* fetchData(action) {
     yield put({type: 'global/FETCHING_DATA', payload: {order, category}});
     let call_name, args;
     if (order === 'trending') {
-        call_name = 'getDiscussionsByTrendingAsync';
+        call_name = 'get_discussions_by_trending';
         args = [
         { tag: category,
           limit: constants.FETCH_DATA_BATCH_SIZE,
           start_author: author,
           start_permlink: permlink}];
     } else if (order === 'trending30') {
-        call_name = 'getDiscussionsByTrending30Async';
+        call_name = 'get_discussions_by_trending30';
         args = [
         { tag: category,
           limit: constants.FETCH_DATA_BATCH_SIZE,
           start_author: author,
           start_permlink: permlink}];
     } else if (order === 'promoted') {
-        call_name = 'getDiscussionsByPromotedAsync';
+        call_name = 'get_discussions_by_promoted';
         args = [
         { tag: category,
           limit: constants.FETCH_DATA_BATCH_SIZE,
           start_author: author,
           start_permlink: permlink}];
     } else if( order === 'active' ) {
-        call_name = 'getDiscussionsByActiveAsync';
+        call_name = 'get_discussions_by_active';
         args = [
         { tag: category,
           limit: constants.FETCH_DATA_BATCH_SIZE,
           start_author: author,
           start_permlink: permlink}];
     } else if( order === 'cashout' ) {
-        call_name = 'getDiscussionsByCashoutAsync';
+        call_name = 'get_discussions_by_cashout';
+        args = [
+        { tag: category,
+          limit: constants.FETCH_DATA_BATCH_SIZE,
+          start_author: author,
+          start_permlink: permlink}];
+    } else if( order === 'payout' ) {
+        call_name = 'get_discussions_by_payout';
         args = [
         { tag: category,
           limit: constants.FETCH_DATA_BATCH_SIZE,
           start_author: author,
           start_permlink: permlink}];
     } else if( order === 'updated' ) {
-        call_name = 'getDiscussionsByActiveAsync';
+        call_name = 'get_discussions_by_active';
         args = [
         { tag: category,
           limit: constants.FETCH_DATA_BATCH_SIZE,
           start_author: author,
           start_permlink: permlink}];
     } else if( order === 'created' || order === 'recent' ) {
-        call_name = 'getDiscussionsByCreatedAsync';
+        call_name = 'get_discussions_by_created';
         args = [
         { tag: category,
           limit: constants.FETCH_DATA_BATCH_SIZE,
           start_author: author,
           start_permlink: permlink}];
     } else if( order === 'by_replies' ) {
-        call_name = 'getRepliesByLastUpdateAsync';
+        call_name = 'get_replies_by_last_update';
         args = [author, permlink, constants.FETCH_DATA_BATCH_SIZE];
     } else if( order === 'responses' ) {
-        call_name = 'getDiscussionsByChildrenAsync';
+        call_name = 'get_discussions_by_children';
         args = [
         { tag: category,
           limit: constants.FETCH_DATA_BATCH_SIZE,
           start_author: author,
           start_permlink: permlink}];
     } else if( order === 'votes' ) {
-        call_name = 'getDiscussionsByVotesAsync';
+        call_name = 'get_discussions_by_votes';
         args = [
         { tag: category,
           limit: constants.FETCH_DATA_BATCH_SIZE,
           start_author: author,
           start_permlink: permlink}];
     } else if( order === 'hot' ) {
-        call_name = 'getDiscussionsByHotAsync';
+        call_name = 'get_discussions_by_hot';
         args = [
         { tag: category,
           limit: constants.FETCH_DATA_BATCH_SIZE,
           start_author: author,
           start_permlink: permlink}];
     } else if( order === 'by_feed' ) { // https://github.com/steemit/steem/issues/249
-        call_name = 'getDiscussionsByFeedAsync';
+        call_name = 'get_discussions_by_feed';
         args = [
         { tag: accountname,
           limit: constants.FETCH_DATA_BATCH_SIZE,
           start_author: author,
           start_permlink: permlink}];
     } else if( order === 'by_author' ) {
-        call_name = 'getDiscussionsByBlogAsync';
+        call_name = 'get_discussions_by_blog';
         args = [
         { tag: accountname,
           limit: constants.FETCH_DATA_BATCH_SIZE,
           start_author: author,
           start_permlink: permlink}];
     } else if( order === 'by_comments' ) {
-        call_name = 'getDiscussionsByCommentsAsync';
+        call_name = 'get_discussions_by_comments';
         args = [
         { limit: constants.FETCH_DATA_BATCH_SIZE,
           start_author: author,
           start_permlink: permlink}];
     } else {
-        call_name = 'getDiscussionsByActiveAsync';
+        call_name = 'get_discussions_by_active';
         args = [{
             tag: category,
             limit: constants.FETCH_DATA_BATCH_SIZE,
@@ -173,11 +181,35 @@ export function* fetchData(action) {
             start_permlink: permlink}];
     }
     try {
-        const data = yield call([api, api[call_name]], ...args);
+        const db_api = Apis.instance().db_api;
+        const data = yield call([db_api, db_api.exec], call_name, args);
         yield put(GlobalReducer.actions.receiveData({data, order, category, author, permlink, accountname}));
     } catch (error) {
         console.error('~~ Saga fetchData error ~~>', call_name, args, error);
         yield put({type: 'global/STEEM_API_ERROR', error: error.message});
+    }
+}
+
+export function* watchApiRequests() {
+    yield* takeEvery('global/FETCH_API', fetchApi);
+}
+export function* fetchApi({payload: {exec, key, reducer, skipLoading = false}}) {
+    const [api, method, ...args] = exec
+    try {
+        const apiInst = Apis.instance()[api];
+        yield put(GlobalReducer.actions.update({key, notSet: Map(),
+            updater: m => m.mergeDeep({loading: true})
+        }))
+        const value = yield skipLoading ? apiInst.exec(method, args) :
+            call([apiInst, apiInst.exec], method, args)
+        let v = fromJS(value)
+        if(reducer) v = v.reduce(...reducer)
+        yield put(GlobalReducer.actions.update({key, notSet: Map(),
+            updater: m => m.mergeDeep({result: v, error: null, loading: false})
+        }))
+    } catch (error) {
+        console.error('~~ Saga fetchApi error ~~>', method, args, error);
+        yield put(GlobalReducer.actions.set({key, value: {error, result: undefined, loading: false}}))
     }
 }
 
