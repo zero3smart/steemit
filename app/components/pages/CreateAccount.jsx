7b2e0f1ba3846ce3b9a32d10665da4b02eceb8f6
kuissoft <fivewisdomzero@gmail.com>
2017-09-02 18:24:24 -0400

    show account status warning on pick account page

--- app/components/pages/CreateAccount.jsx
+++ app/components/pages/CreateAccount.jsx
@@ -10,6 +10,7 @@ import runTests from 'app/utils/BrowserTests';
 import GeneratedPasswordInput from 'app/components/elements/GeneratedPasswordInput';
 import {saveCords} from 'app/utils/ServerApiClient';
 import {api} from 'steem';
+import { Link } from 'react-router';
 
 class CreateAccount extends React.Component {
 
@@ -217,7 +218,7 @@ class CreateAccount extends React.Component {
                     <div className="callout alert">
                         <p>It looks like your sign up request is not approved yet or you already created an account.<br />
                            Please try again later or contact <a href="mailto:support@steemit.com">support@steemit.com</a> for the status of your request.<br />
-                           If you didn't submit your sign up application yet, <a href="/pick_account">apply now</a>!
+                           If you didn't submit your sign up application yet, <Link to="/pick_account">apply now</Link>!
                         </p>
                     </div>
                 </div>
@@ -286,7 +287,7 @@ class CreateAccount extends React.Component {
                             </div>
                             { // TODO: remove this after May 20th
                                 this.state.account_has_keys_warning && <div className="warning">
-                                    Please note: due to recent security changes if you chosen a password before during signup, this one below will override it  this is the one you need to save.
+                                    Please note: due to recent security changes if you chosen a password before during signup, this one below will override it — this is the one you need to save.
                                 </div>
                             }
                             <GeneratedPasswordInput onChange={this.onPasswordChange} disabled={loading} showPasswordString={this.state.showPass} />
